66a478356396ef9edf8317e1d89be8cde7264d3a
TinTinLatt <tintinlatt23@gmail.com>
2023-02-02 01:22:55 +0900

    all my files

---
+++ js/common.js
@@ -0,0 +1,18 @@
+const sendData = (path, data) => {
+    console.log(data);
+    fetch(path, {
+        method: 'post',
+        headers: new Headers({'Content-Type': 'application/json'}),
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => processData(data));
+}
+const processData = (data) => {
+    console.log(data);
+}
+const showFormError = (err) => {
+    let errorEle = document.querySelector('.error');
+    errorEle.innerHTML = err;
+    errorEle.classList.add('show')
+}
